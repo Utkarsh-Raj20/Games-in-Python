e191a03e5fa2133966ce202a800f7c0e9d0782ef
Utkarsh-Raj20 <utkarshraj.code@gmail.com>
2022-03-29 17:31:03 +0530

    better opponent logic and code fixex

--- Ping Pong/ping pong.py
+++ Ping Pong/ping pong.py
@@ -13,6 +13,11 @@ White = (255, 255, 255)
 VEL = 7
 
 
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf", 32)
+
+
 class Player:
     def __init__(self, x) -> None:
         self.x = x
@@ -23,14 +28,24 @@ class Player:
         self.body = pygame.Rect(self.x, self.y, 10, 140)
         pygame.draw.rect(WIN, (200, 200, 200), self.body)
 
+    def reset_player(self):
+        self.body = pygame.Rect(self.x, HEIGHT / 2 - 70, 10, 140)
+        pygame.draw.rect(WIN, (200, 200, 200), self.body)
 
-def draw_window(one, two, ball):
+
+def draw_window(one, two, ball, p_score, o_score):
     WIN.fill(pygame.Color("grey12"))
 
     one.draw()
     two.draw()
     pygame.draw.ellipse(WIN, (200, 200, 200), ball)
     pygame.draw.aaline(WIN, (200, 200, 200), (WIDTH / 2, 0), (WIDTH / 2, HEIGHT))
+
+    player_score_display = game_font.render(f"{p_score}", False, (200, 200, 200))
+    WIN.blit(player_score_display, (WIDTH / 2 - 16 - 32, HEIGHT / 2 - 16))
+
+    opponent_score_display = game_font.render(f"{o_score}", False, (200, 200, 200))
+    WIN.blit(opponent_score_display, (WIDTH / 2 + 16 + 20, HEIGHT / 2 - 16))
 
     pygame.display.update()
 
@@ -43,13 +58,16 @@ def handle_player_one(key, one):
 
 
 def main() -> None:
+    global player_score, opponent_score
 
     player_one = Player(10)
     opponent = Player(WIDTH - 20)
     ball = pygame.Rect(WIDTH / 2 - 15, HEIGHT / 2 - 15, 30, 30)
 
-    ball_speed_x = 7 * random.choice((1, -1))
-    ball_speed_y = 7 * random.choice((1, -1))
+    ball_speed_x = 6 * random.choice((1, -1))
+    ball_speed_y = 6 * random.choice((1, -1))
+
+    opponent_speed = 7
 
     running = True
     clock = pygame.time.Clock()
@@ -67,8 +85,16 @@ def main() -> None:
 
         if ball.top <= 0 or ball.bottom >= HEIGHT:
             ball_speed_y *= -1
-        if ball.left <= 0 or ball.right >= WIDTH:
-            
+        if ball.left <= 0:
+            opponent_score += 1
+            # . BALL RESTART
+            player_one.y = HEIGHT / 2 - 70
+            ball.center = (WIDTH / 2, HEIGHT / 2)
+            ball_speed_x *= random.choice((1, -1))
+            ball_speed_y *= random.choice((1, -1))
+
+        if ball.right >= WIDTH:
+            player_score += 1
             # . BALL RESTART
             ball.center = (WIDTH / 2, HEIGHT / 2)
             ball_speed_x *= random.choice((1, -1))
@@ -79,18 +105,17 @@ def main() -> None:
 
         # ~ Opponent Movement
 
-        if ball_speed_y > 0 and opponent.body.bottom <= HEIGHT - 5:
-            opponent.y += 7
-        if ball_speed_y < 0 and opponent.body.top >= 5:
-            opponent.y -= 7
+        if ball.top > opponent.body.top and opponent.body.bottom <= HEIGHT - 5:
+            opponent.y += opponent_speed
+        if ball.top < opponent.body.top and opponent.body.top >= 5:
+            opponent.y -= opponent_speed
         if opponent.x <= 0:
             opponent.x = 0
 
         key_pressed = pygame.key.get_pressed()
 
         handle_player_one(key_pressed, player_one)
-        # handle_opponent(opponent, ball)
-        draw_window(player_one, opponent, ball)
+        draw_window(player_one, opponent, ball, player_score, opponent_score)
         pygame.display.update()
 
     pygame.display.quit()
